11b6b42bc668e6618d1940c91d9e8470938e6964
MichaelZavalo <mihailzavalo@yandex.ru>
2020-10-07 19:57:48 +0300

    feat: add components and router

---
+++ src/components/Congratulations.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+export default class Congratulations extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <div className='congratulations'>
+                {Congratulations}
+            </div>
+        )
+    }
+}
